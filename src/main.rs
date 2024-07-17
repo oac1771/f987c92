4a46446fd3ae2237a7e09ca9d1cc88dfdc4005cd
Omar Carey <omarcarey@omars-mbp-2.lan>
2024-07-17 14:50:54 -0500

    add state to app and save movie payload on post request

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,53 @@
+use std::collections::HashMap;
 use axum::{
+    extract::{Json, State},
     routing::{get, post},
-    extract::Json,
     Router,
 };
 use serde::Deserialize;
+use serde_json::{Value, json};
+use uuid::Uuid;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Movie {
     id: String,
     name: String,
     year: u16,
-    was_good: bool
+    was_good: bool,
+}
+
+#[derive(Clone, Debug)]
+struct AppState {
+    db: HashMap<String, Movie>
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            db: HashMap::new()
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
+
+    let state = AppState::new();
     // build our application with a single route
     let app = Router::new()
-    .route("/health", get(|| async { "up" }))
-    .route("/movie", post(post_movie));
+        .route("/health", get(|| async { "up" }))
+        .route("/movie", post(post_movie))
+        .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn post_movie(Json(movie): Json<Movie>) {
-    println!("{:?}", movie);
+// #[axum_macros::debug_handler]
+async fn post_movie(State(mut state): State<AppState>, Json(movie): Json<Movie>) -> Json<Value> {
+    let movie_id = Uuid::new_v4().to_string();
+    state.db.insert(movie_id.clone(), movie);
+
+    Json(json!({"id": movie_id}))
 }
